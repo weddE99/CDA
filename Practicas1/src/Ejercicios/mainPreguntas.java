2007c3bab6a1ed5a365a5ffe61fbb7fe27ea94c9
weddE99 <54249487+weddE99@users.noreply.github.com>
2019-10-08 11:19:21 -0300

    Add files via upload

--- Practicas1/src/Ejercicios/mainPreguntas.java
+++ Practicas1/src/Ejercicios/mainPreguntas.java
@@ -4,15 +4,19 @@ import java.util.Scanner;
 
 public class mainPreguntas {
 	static char []caract = {'+','-','*','/'};
-	static int cant = 1;
+	static int cant = 5;
 	
 	public static void main(String []args) {
+		mostrar();
+	}
+	
+	public static void mostrar() {
 		Scanner scan = new Scanner(System.in);
 		for(int i=0; i<cant; i++) {
 			int a = (int)Math.floor(Math.random()*10+1);
 			int b = (int)Math.floor(Math.random()*10+1);
 			int c = (int)Math.floor(Math.random()*3+1);
-			System.out.print("Cuanto es "+a+""+caract[c]+""+b+": ");
+			System.out.print("Cuanto es "+a+caract[c]+b+": ");
 			int d = scan.nextInt();
 			System.out.println("=================================");
 			Preguntas pregunta = new Preguntas(a,b,c+1,d);
